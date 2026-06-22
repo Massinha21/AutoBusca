a7db683aa9bdb4cbf664f5a9834b43cc966ff6a1
Massinha21 <ceribelipedro@gmail.com>
2026-06-22 11:13:53 -0300

    chore: atualiza cache para nova imagem da logo

--- public/sw.js
+++ public/sw.js
@@ -4,7 +4,7 @@
 // Responsável por prover cache offline para o App Shell (estáticos) e habilitar PWA.
 //
 
-const CACHE_NAME = "autobusca-cache-v7";
+const CACHE_NAME = "autobusca-cache-v8";
 
 const PRECACHE_ASSETS = [
   "/",
