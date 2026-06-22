603694afa54f76c1877a98d4059f2e6a87731818
Massinha21 <ceribelipedro@gmail.com>
2026-06-22 11:27:29 -0300

    style: atualiza frase principal, cor e tamanho

--- public/sw.js
+++ public/sw.js
@@ -4,7 +4,7 @@
 // Responsável por prover cache offline para o App Shell (estáticos) e habilitar PWA.
 //
 
-const CACHE_NAME = "autobusca-cache-v10";
+const CACHE_NAME = "autobusca-cache-v11";
 
 const PRECACHE_ASSETS = [
   "/",
